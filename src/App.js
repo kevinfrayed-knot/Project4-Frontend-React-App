4906a4747be6ba168f71b6795343a982c9f16a75
kevinfrayed-knot <kevin@frayed-knot.com>
2024-12-11 14:27:51 -0600

    updated directory structure inside src, updated app.js to include routing for all pages.

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/Auth/LoginPage';
+import RegisterPage from './components/Auth/RegisterPage';
+import CategoryPage from './components/CategoryPage/CategoryPage';
+import NewQuestionForm from './components/CategoryPage/NewQuestionForm';
+import QuestionDetailPage from './components/QuestionDetailPage/QuestionsDetailPage';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/categories/new" element={<NewQuestionForm />} />
+        <Route path="/questions/:id" element={<QuestionDetailPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+
+
+import React from 'react';
+
+function Home() {
+  return <h1>Welcome to the Forum App</h1>;
+}
+
+export default Home;
