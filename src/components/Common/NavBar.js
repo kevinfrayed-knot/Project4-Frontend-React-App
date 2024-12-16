bf449b8305aa7e028e7e19694d154e2bcd8f518f
kevinfrayed-knot <kevin@frayed-knot.com>
2024-12-16 15:03:21 -0600

    added logout link with conditional render so that it only displays when a useer is succesfully logged in.

--- src/components/Common/NavBar.js
+++ src/components/Common/NavBar.js
@@ -1,24 +1,49 @@
 
 
-// src/components/Common/NavBar.js
-
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 
 const NavBar = () => {
+  const { isAuthenticated, logout } = useContext(AuthContext);
+
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/categories">View Categories</Link></li>
-        <li><Link to="/categories/new">Create New Category</Link></li>
-        <li><Link to="/questions/new">Create New Question</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/categories">View Categories</Link>
+        </li>
+        <li>
+          <Link to="/categories/new">Create New Category</Link>
+        </li>
+        <li>
+          <Link to="/questions/new">Create New Question</Link>
+        </li>
+        {!isAuthenticated && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+        {isAuthenticated && (
+          <li>
+            <button onClick={logout} style={{ background: 'none', border: 'none', color: 'blue', cursor: 'pointer' }}>
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
 };
 
 export default NavBar;
+
 
