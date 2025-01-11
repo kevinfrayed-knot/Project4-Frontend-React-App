98fa3f80f76c9a9f71ce2f75f32c517518b4b6a5
kevinfrayed-knot <kevin@frayed-knot.com>
2025-01-10 21:39:04 -0600

    removed new question and new category links from NavBar.js

--- src/components/Common/NavBar.js
+++ src/components/Common/NavBar.js
@@ -11,35 +11,26 @@ const NavBar = () => {
 
   return (
     <nav className="navbar">
-      <ul className="nav-links">
-        <li className="nav-item">
-          <Link to="/categories/new" className="nav-link">New Category</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/questions/new" className="nav-link">New Question</Link>
-        </li>
-
-        {isAuthenticated ? (
+      {isAuthenticated ? (
+        <ul className="nav-links">
           <li className="nav-item">
             <button onClick={logout} className="nav-button">
               Logout
             </button>
           </li>
-        ) : (
-          <>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/register" className="nav-link">Register</Link>
-            </li>
-          </>
-        )}
-      </ul>
+        </ul>
+      ) : (
+        <ul className="nav-links">
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/register" className="nav-link">Register</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
 
 export default NavBar;
-
-
